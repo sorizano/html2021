5a1233038b25bd4ba1f17ea2f7c8454cf5f790ab
Sergio Victor Orizano Salvador <sorizano@continental.edu.pe>
2021-04-13 23:21:00 -0500

    guardando datos en firebase

--- 0704/index.js
+++ 0704/index.js
@@ -1,10 +1,20 @@
+const db = firebase.firestore();
+
+
 const taskForm = document.getElementById('task-form');
 
-taskForm.addEventListener('submit', e => {
+taskForm.addEventListener('submit', async(e) => {
     e.preventDefault();
 
     const title = taskForm['task-title'].value;
     const descriprion = taskForm['task-desciption'].value;
+
+    const response = await db.collection('tasks').doc().set({
+        title,
+        descriprion
+    })
+
+    console.log(response)
 
     console.log(title, descriprion)
 })
