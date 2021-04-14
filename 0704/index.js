8eecbd32aa5940ff752f55f67b57ff9378547eac
Sergio Victor Orizano Salvador <sorizano@continental.edu.pe>
2021-04-13 23:09:29 -0500

    submiting

--- 0704/index.js
+++ 0704/index.js
@@ -1,1 +1,6 @@
-console.log('works!!')
+const taskForm = document.getElementById('task-form');
+
+taskForm.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('sucmiting')
+})
