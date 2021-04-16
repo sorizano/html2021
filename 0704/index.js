989b98fed9610d33bd5a48b88c9dd4dfb1f25773
Sergio <svorizano@gmail.com>
2021-04-15 20:08:46 -0500

    final

--- 0704/index.js
+++ 0704/index.js
@@ -3,6 +3,9 @@ const db = firebase.firestore();
 const taskForm = document.getElementById("task-form");
 const taskContainer = document.getElementById("tasks-container");
 
+let editStatus = false;
+let id = "";
+
 const saveTask = (title, description) =>
     db.collection("tasks").doc().set({
         title,
@@ -13,6 +16,12 @@ const getTasks = () => db.collection("tasks").get();
 
 const onGetTasks = (callback) => db.collection("tasks").onSnapshot(callback);
 
+const deleteTask = id => db.collection("tasks").doc(id).delete();
+
+const getTask = (id) => db.collection("tasks").doc(id).get();
+
+const updateTask = (id, updateTask) => db.collection("tasks").doc(id).update(updateTask);
+
 window.addEventListener("DOMContentLoaded", async(e) => {
     onGetTasks((querySnapshot) => {
         taskContainer.innerHTML = "";
@@ -20,17 +29,41 @@ window.addEventListener("DOMContentLoaded", async(e) => {
             console.log(doc.data())
 
             const task = doc.data();
+            task.id = doc.id;
 
             taskContainer.innerHTML += `<div class="card card-body mt-2 border-primary">
                
                 <h3 class="h5">${task.title}</h3>
                 <p>${task.description}</p>
                 <div >
-                    <button class="btn btn-primary">Delete</button>
-                    <button class="btn btn-secondary">Edit</button>
+                    <button class="btn btn-primary btn-delete" data-id="${task.id}">Delete</button>
+                    <button class="btn btn-secondary btn-edit" data-id="${task.id}">Edit</button>
                 </div>
     
-            </div>`
+            </div>`;
+
+            const btnsDelete = document.querySelectorAll(".btn-delete");
+            btnsDelete.forEach(btn => {
+                btn.addEventListener("click", async(e) => {
+                    await deleteTask(e.target.dataset.id)
+                })
+            });
+
+            const btnsEdit = document.querySelectorAll(".btn-edit");
+            btnsEdit.forEach(btn => {
+                btn.addEventListener("click", async(e) => {
+                    const doc = await getTask(e.target.dataset.id);
+                    const task = doc.data();
+
+                    editStatus = true;
+                    id = doc.id;
+
+                    taskForm["task-title"].value = task.title;
+                    taskForm["task-description"].value = task.description;
+                    taskForm["btn-task-form"].innerText = "Update";
+                })
+            })
+
         });
     });
 });
@@ -41,7 +74,18 @@ taskForm.addEventListener("submit", async(e) => {
     const title = taskForm["task-title"];
     const description = taskForm["task-description"];
 
-    await saveTask(title.value, description.value);
+    if (!editStatus) {
+        await saveTask(title.value, description.value);
+    } else {
+        await updateTask(id, {
+            title: title.value,
+            description: description.value
+        });
+
+        editStatus = false;
+        id = "";
+        taskForm["btn-task-form"].innerText = "Save";
+    }
 
     taskForm.reset();
     title.focus();
