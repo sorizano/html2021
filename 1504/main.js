82f9d58efe8a1112352fa132e19c11a71b4c596d
Sergio Victor Orizano Salvador <sorizano@continental.edu.pe>
2021-04-20 18:44:36 -0500

    registrando en firebase V1.3.3

--- 1504/main.js
+++ 1504/main.js
@@ -3,8 +3,20 @@ const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const signupEmail = document.querySelector("#signup-email").value;
-    const signupPassword = document.querySelector("#signup-password").value;
+    const email = document.querySelector("#signup-email").value;
+    const password = document.querySelector("#signup-password").value;
 
-    console.log(signupEmail, signupPassword)
+
+    auth
+        .createUserWithEmailAndPassword(email, password)
+        .then(userCredential => {
+
+            signupForm.reset();
+
+            const sergio = document.getElementById('#signupModal') // relatedTarget
+            myModal.show(sergio)
+
+
+            console.log("sign up")
+        })
 })
